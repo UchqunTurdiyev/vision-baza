1178ad24a5b1bbe4e16be5032c92f7bfec34ed23
Uchqun Turdiyev <uchqunjonturdiev@gmail.com>
2025-11-20 19:01:55 +0500

    Versel purches event

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import Script from "next/script";
 import "./globals.css";
 import type { Metadata } from "next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 
 const PIXEL_ID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID ?? '874895024822350';
@@ -102,6 +103,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           />
         </noscript>
         {children}
+        <SpeedInsights />
         </body>
     </html>
   );
